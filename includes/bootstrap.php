91b3be8d890e78a677ccdf0b5b9957724bedbbc5
okamoto toshiyuki <GitHub>
2025-09-21 15:12:28 +0900

    feat(core): bootstrap & guarded loader (M0 scaffold)

--- includes/bootstrap.php
+++ includes/bootstrap.php
@@ -1,1 +1,42 @@
-<?php // bootstrap placeholder ?>
+<?php
+
+namespace MinpakuSuite;
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+class Bootstrap
+{
+    public static function init()
+    {
+        self::init_hooks();
+        self::init_rest_api();
+        self::init_acf();
+    }
+
+    public static function activate()
+    {
+        // Activation logic will be implemented in future milestones
+    }
+
+    public static function deactivate()
+    {
+        // Deactivation logic will be implemented in future milestones
+    }
+
+    private static function init_hooks()
+    {
+        // WordPress hooks initialization will be implemented in future milestones
+    }
+
+    private static function init_rest_api()
+    {
+        // REST API initialization will be implemented in future milestones
+    }
+
+    private static function init_acf()
+    {
+        // ACF (Advanced Custom Fields) initialization will be implemented in future milestones
+    }
+}
